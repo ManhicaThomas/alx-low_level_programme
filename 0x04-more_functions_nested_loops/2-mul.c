13871058b47752d3d5933eb9e8b2491aad0d9735
ManhicaThomas <thomasmanchica@gmail.com>
2023-10-12 15:31:24 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 /**
  * mul - func checks for checks for checks for a digit (0 through).
  * @a: a - Variable
- * @b: b - variable 
+ * @b: b - Variable 
  * Return: Always 0.
  */
 int mul(int a, int b)
